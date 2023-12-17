796502f231b09d8006cffc80653f22955217d807
Dev Patel <142797423+Devapatel0603@users.noreply.github.com>
2023-12-17 11:12:35 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,16 +2,10 @@ from __init__ import db, login_manager
 from flask_login import UserMixin
 
 @login_manager.user_loader
-def load_user(user_id):
-    return Users.query.get(int(user_id))
+def load_user(email):
+    return Users.query.get(email))
 
 class Users(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.Integer, nullable=False)
-
-class Message(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=False, nullable=False)
-    email = db.Column(db.String(20), unique=False, nullable=False)
+    email = db.Column(db.String(20), nullable=False, primary_key=True)
+    password = db.Column(db.String(60), nullable=False)
